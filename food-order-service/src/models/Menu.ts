b3dbc3e192303b78cbe653bfbd8ff3b40531aa47
Rokas Kašinskas <yourokas@gmail.com>
2023-03-19 01:57:04 +0200

    feat: create menu model

--- food-order-service/src/models/Menu.ts
+++ food-order-service/src/models/Menu.ts
@@ -1,1 +1,26 @@
-import React from 'react';
+import { makeObservable, observable, computed, action, runInAction } from 'mobx';
+
+export interface IMenuItem {
+    id: string;
+    name: string;
+    price: number;
+}
+
+export class Menu implements IMenuItem {
+    @observable public id: string;
+    @observable public name: string;
+    @observable public price: number;
+
+
+    public constructor(data: IMenuItem) {
+        makeObservable(this);
+        this.initFromData(data);
+    }
+
+    @action
+    public initFromData(data: IMenuItem) {
+        this.id = data.id;
+        this.name = data.name;
+        this.price = data.price;
+    }
+}
---
+++ food-order-service/src/stores/MenuStore.ts
@@ -0,0 +1,18 @@
+import { makeObservable, observable, computed, action, runInAction } from 'mobx';
+import { Menu } from '../models/Menu';
+
+export class MenuStore {
+    @observable public Menu: Menu[] = [];
+
+    public constructor() {
+        makeObservable(this);
+    }
+
+    @action AddToStore(data: Menu) {
+        
+    }
+
+    @action DeleteMenuItem() {
+        
+    }
+}
